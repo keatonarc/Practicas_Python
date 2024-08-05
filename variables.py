1857a90cf3e1a3680fd36085b4317a2297ab17d4
keatonarc <matiasaguielra@gmail.com>
2024-08-04 20:38:10 -0400

    actualizacion

---
+++ variables.py
@@ -0,0 +1,20 @@
+#Variable tipo entero
+a=10
+b=20
+print(a) 
+print(b)
+#Variable flotantes
+c=1.5
+d=3.7
+suma = c + d
+print(suma)
+#variable tipo string
+nombre = "Roberto"
+apellido= 'Hernandez'
+fullName = nombre + " " + apellido
+print(fullName)
+# Variable tipo boolean
+lampara=True
+print("la lampara esta encendida",lampara)
+lampara=False
+print("la lampara esta apagada",lampara)
